2d622bb5b2d2cf87a65c3797d243b7dd48fae2af
纵歌 <xw.wang@catontechnology.com>
2024-11-21 17:31:34 +0800

    mod quick_chat_c to struct

--- quick_chat_c/src/qc_client.rs
+++ quick_chat_c/src/qc_client.rs
@@ -2,28 +2,34 @@ use anyhow::{Context, Result};
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
 
-pub struct QcClient {
-    stream: TcpStream,
-}
+pub struct QcClient {}
 
 impl QcClient {
-    pub async fn new(address: &str) -> Result<Self> {
+    pub async fn open(address: &str) -> Result<()> {
         let stream = TcpStream::connect(address)
             .await
             .context("Failed to connect qc server")?;
 
         println!("connect to: {}", address.to_string());
-        Ok(QcClient { stream })
-    }
+        let (reader, mut writer) = stream.into_split();
 
-    pub async fn start(&self) -> result<()> {
-        let (reader, mut writer) = self.stream.into_split();
-        let mut reader_stdin = BuffReader::new(io::stdin()).lines;
-        let mut reader_qc = BuffReader::new(reader).lines;
+        let mut stdin_reader = BufReader::new(io::stdin()).lines();
+        let mut server_reader = BufReader::new(reader).lines();
+
         loop {
-tokio::select! {
-    
-}
+            tokio::select! {
+                Ok(line) = stdin_reader.next_line() => {
+                    if let Some(line) = line {
+                        writer.write_all(line.as_bytes()).await?;
+                        writer.write_all(b"\n").await?;
+                    }
+                }
+                Ok(line) = server_reader.next_line() => {
+                    if let Some(line) = line {
+                        println!("Server: {}", line);
+                    }
+                }
+            }
         }
     }
 }
